a91c6be17bcf36a155ce27a018bbac279fe1ab86
Muhammad Arabi <khanarabi80@gmail.com>
2025-06-13 10:16:55 +0500

    Delete 3rd Semester/oop/BankDatabase.java

--- 3rd Semester/oop/BankDatabase.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDateTime;
-abstract class Transactions{
-    private LocalDateTime date;
-    private double amount;
-
-    //details Method
-    public abstract String getDetails(); 
-}
